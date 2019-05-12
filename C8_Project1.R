cb320d4c3fb6859590dd15f4b432aa9040817222
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-05-12 11:26:33 -0500

    Created initial script, updated .gitignore file

---
+++ C8_Project1.R
@@ -0,0 +1,40 @@
+# Coursera Data Science Specialization Course 8 Project 1 Script----------------
+# Predicting exercise quality with in the "Weight Lifting Exercises" dataset
+
+
+# Acknowledgement for this dataset goes to:
+# Velloso, E.; Bulling, A.; Gellersen, H.; Ugulino, W.; Fuks, H. Qualitative
+# Activity Recognition of Weight Lifting Exercises. Proceedings of 4th
+# International Conference in Cooperation with SIGCHI (Augmented Human '13) .
+# Stuttgart, Germany: ACM SIGCHI, 2013.
+#
+# Additional information on their project can be found at the link below:
+# http://groupware.les.inf.puc-rio.br/har
+
+
+# The purpose of this script is to complete the basic requirements behind the
+# project 1 peer-graded assignment which is part of the Practical Machine
+# Learning course from Johns Hopkins University within the Data Science
+# Specialization on Coursera.
+#
+# The instructions say to build a model that predicts the "classe" variable
+# using any of the other variables in the dataset.
+#
+# The input for this document is the WLE dataset which comes from the URL below.
+# The script leaves behind the data object and the model object that best
+# predicted the outcome, but the primary purpose is performing the analysis
+# which can later be summarized in a markdown file and report.
+
+
+#library(tidyverse)
+
+
+# Part 0) Function definitions--------------------------------------------------
+
+
+
+
+
+
+
+
